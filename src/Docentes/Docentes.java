54ca3b259cff6436172632d4ef7cb48d53781aa0
Jhadri <joadrojasro@est.utn.ac.cr>
2025-07-03 15:04:12 -0600

    se crea la clase docentes
    se crea la clase docentes con sus respectos metodos, atributos y to string respectivo

---
+++ src/Docentes/Docentes.java
@@ -0,0 +1,26 @@
+package Docentes;
+import Personas.Persona;
+
+  public class Docentes extends Persona {
+    private String departamento;
+    
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public Docentes(int cedula, String nombre, String departamento) {
+        
+        this.nombre = nombre;
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Docentes{" + "departamento=" + departamento + '}';
+    }
+    
+    }  
