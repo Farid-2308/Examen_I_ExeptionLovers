ed4bb5e4a02f906733ec39f90010886506553e34
Jhadri <joadrojasro@est.utn.ac.cr>
2025-07-03 15:06:14 -0600

    se crea la lista de docentes
    se crea la lista de docentes y se actualiza la clase lista base

---
+++ src/Docentes/ListaDocente.java
@@ -0,0 +1,22 @@
+package Docentes;
+import Listas.Lista;
+
+
+public class ListaDocente implements Lista{
+
+    @Override
+    public boolean find(Object t) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean add(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean remove(Object t) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
